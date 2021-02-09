055e3f1d042468184b001273e70c89e41d513df4
nicholas <nicholas.hammond@form3.tech>
2021-02-09 02:38:57 +0000

    Added start of promise

---
+++ pkg/core/core2/optional.go
@@ -0,0 +1,115 @@
+package core2
+
+import (
+	"fmt"
+)
+
+// Is a container for zero or one element of a given type
+type Optional struct {
+	value    T
+	hasValue bool
+}
+
+type FlatMapFunc func(value T) Optional
+type MapFunc func(value T) T
+type PredicateFunc func(value T) bool
+
+type MapSome func(value T) R
+type MapNone func() R
+
+type T interface{}
+
+var EmptyError = fmt.Errorf("is empty")
+
+// Returns the option's value.
+func (o Optional) Get() (T, error) {
+	if o.IsNone() {
+		return nil, EmptyError
+	}
+	return o.value, nil
+}
+
+// Returns true if the option is in a None state, false otherwise.
+func (o Optional) IsNone() bool {
+	return !o.hasValue
+}
+
+// Returns true if this option is nonempty and the predicate p returns true when applied to this Optional's value. Otherwise, returns false.
+func (o Optional) Exists(f PredicateFunc) bool {
+	if !o.IsSome() {
+		return false
+	}
+	return f(o.value)
+}
+
+// Returns true if the option is a Some state, false otherwise.
+func (o Optional) IsSome() bool {
+	return o.hasValue
+}
+
+func (o Optional) IfSome(f func(value interface{})) Optional {
+	if o.IsSome() {
+		f(o.value)
+	}
+	return o
+}
+
+func (o Optional) IfNone(f func()) Optional {
+	if o.IsNone() {
+		f()
+	}
+	return o
+}
+
+// Returns the option's value if the option is nonempty, otherwise return the result of evaluating default.
+func (o Optional) GetOrElse(defaultValue interface{}) interface{} {
+	if o.IsNone() {
+		return defaultValue
+	}
+	return o.value
+}
+
+// Returns this Optional if it is nonempty and applying the predicate p to this Optional's value returns true. Otherwise, return None.
+func (o Optional) Filter(f PredicateFunc) Optional {
+	if o.IsSome() && o.Exists(f) {
+		return o
+	}
+	return None()
+}
+
+// Returns the result of applying f to this Optional's value if this Optional is nonempty. Returns None if this Optional is empty.
+func (o Optional) FlatMap(f FlatMapFunc) Optional {
+	if o.IsNone() {
+		return o
+	}
+	return f(o.value)
+}
+
+// Returns a Some containing the result of applying f to this Optional's value if this Optional is nonempty. Otherwise return None.
+func (o Optional) Map(f MapFunc) Optional {
+	if o.IsNone() {
+		return o
+	}
+	return Some(f(o.value))
+}
+
+func (o Optional) Match(some MapSome, none MapNone) R {
+	if o.IsNone() {
+		return none()
+	}
+	return some(o.value)
+}
+
+func None() Optional {
+	return Optional{
+		value:    nil,
+		hasValue: false,
+	}
+}
+
+func Some(value interface{}) Optional {
+	return Optional{
+		value:    value,
+		hasValue: true,
+	}
+}
--- pkg/core/core2/promise.go
+++ pkg/core/core2/promise.go
@@ -7,10 +7,77 @@ type Any interface {
 
 type Future interface {
 	Get() Result
-	OnSuccess(func(value Any) Result)
-	OnFailure(func(err error) Result)
+	OnSuccess(func(value Any))
+	OnFailure(func(err error))
 	Then(func(value Any) Result) Future
 	Alt(that Future) Future
+}
+
+// verify future implements Future interface
+var _ Future = (*future)(nil)
+
+type future struct {
+	resultChan chan Result
+}
+
+func (receiver *future) Get() Result {
+	result := <- receiver.resultChan
+	go func() {
+		receiver.resultChan <- result
+	}()
+	return result
+}
+
+func (receiver *future) OnSuccess(f func(value Any)) {
+	go func() {
+		result := receiver.Get()
+		result.IfSuccess(f)
+	}()
+}
+
+func (receiver *future) OnFailure(f func(err error)) {
+	go func() {
+		result := receiver.Get()
+		result.IfFailure(f)
+	}()
+}
+
+func (receiver *future) Then(f func(value Any) Result) Future {
+	return NewFuture(func() Result {
+		result := receiver.Get()
+		if result.IsSuccess() {
+			return f(result.value)
+		}
+		return result
+	})
+}
+
+func tryCompleteWith(p Promise, f Future) {
+	go func() {
+		f.Get().IfSuccess(func(value Any) {
+			p.TrySuccess(value)
+		}).IfFailure(func(err error) {
+			p.TryFailure(err)
+		})
+	}()
+}
+
+func (receiver *future) Alt(that Future) Future {
+	p := NewPromise()
+	tryCompleteWith(p, receiver)
+	tryCompleteWith(p, that)
+	return p.Future()
+}
+
+func NewFuture(f func() Result) Future  {
+	resultChan := make(chan Result)
+	go func() {
+		result := f()
+		resultChan <- result
+	}()
+	return & future{
+		resultChan: resultChan,
+	}
 }
 
 type Promise interface {
@@ -19,6 +86,48 @@ type Promise interface {
 	TryFailure(err error) bool
 }
 
+// verify promise implements Promise interface
+var _ Promise = (*promise)(nil)
+
+
+type promise struct {
+	completed bool
+	resultChan chan Result
+}
+
+func (p *promise) Future() Future {
+	return NewFuture(func() Result {
+		result := <- p.resultChan
+		return result
+	})
+}
+
+func (p *promise) TrySuccess(value Any) bool {
+	if !p.completed {
+		go func() {
+			p.resultChan <- Success(value)
+		}()
+		return true
+	}
+	return false
+}
+
+func (p *promise) TryFailure(err error) bool {
+	if !p.completed {
+		go func() {
+			p.resultChan <- Failure(err)
+		}()
+		return true
+	}
+	return false
+}
+
+func NewPromise() Promise {
+	return &promise {
+		completed: false,
+		resultChan: make(chan Result),
+	}
+}
 
 // https://www.promisejs.org/implementing/
 
