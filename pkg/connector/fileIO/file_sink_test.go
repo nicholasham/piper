657a58aa204bf39bf63e808e332d4e2d8a069957
nicholasham <nicholas.hammond@form3.tech>
2021-02-11 12:31:24 +0000

    Added file test

--- pkg/connector/fileIO/file_sink_test.go
+++ pkg/connector/fileIO/file_sink_test.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"github.com/nicholasham/piper/pkg/stream"
 	"github.com/nicholasham/piper/pkg/stream/source"
+	"strings"
 	"testing"
 
 	"github.com/nicholasham/piper/pkg/connector/fileIO/testfs"
-	"github.com/nicholasham/piper/pkg/types/list"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,14 +15,14 @@ func TestFileSink(t *testing.T) {
 	fs := testfs.New(t, "FileSinkTest")
 	defer fs.CleanUp()
 
-	expectedLines := list.OfStrings("a\n", "b\n", "c\n", "d\n", "e\n", "f\n")
+	expectedLines := []string {"a\n", "b\n", "c\n", "d\n", "e\n", "f\n"}
 
 	t.Run("Write lines to file", func(t *testing.T) {
 
 		targetFile := fs.GetPath("test.txt")
 
 		result := source.
-			List(expectedLines...).
+			List("a\n", "b\n", "c\n", "d\n", "e\n", "f\n").
 			Map(ByteString).
 			RunWith(context.Background(), ToPath(targetFile, Create))
 
@@ -30,7 +30,7 @@ func TestFileSink(t *testing.T) {
 
 		assert.NoError(t, err)
 		assert.Equal(t, stream.NotUsed, value)
-		assert.Equal(t, list.ToString(expectedLines, ""), fs.ReadFileContents(targetFile))
+		assert.Equal(t, strings.Join(expectedLines, ""), fs.ReadFileContents(targetFile))
 	})
 
 }
