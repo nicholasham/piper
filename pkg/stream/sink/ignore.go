137b4f34c565a1fa155acde4d60583f9f11084dc
nicholasham <nicholas.hammond@form3.tech>
2021-02-11 15:58:00 +0000

    Added ignore

--- pkg/stream/sink/ignore.go
+++ pkg/stream/sink/ignore.go
@@ -16,14 +16,18 @@ func (i *ignoreSinkStageLogic) OnUpstreamStart(_ stream.SinkStageActions) {
 }
 
 func (i *ignoreSinkStageLogic) OnUpstreamReceive(element stream.Element, actions stream.SinkStageActions) {
-	element.WhenError(actions.FailStage)
+	element.WhenError(func(err error) {
+		actions.FailStage(err)
+		i.promise.TryFailure(err)
+	})
 }
 
-func (i *ignoreSinkStageLogic) OnUpstreamFinish(_ stream.SinkStageActions) {
+func (i *ignoreSinkStageLogic) OnUpstreamFinish(actions stream.SinkStageActions) {
+	i.promise.TrySuccess(stream.NotUsed)
 }
 
 func ignoreStage() stream.SinkStage {
-	return stream.Sink(createHeadOptionLogic)
+	return stream.Sink(createIgnoreStageLogic)
 }
 
 func createIgnoreStageLogic(attributes *stream.StageAttributes) (stream.SinkStageLogic, *core.Promise) {
